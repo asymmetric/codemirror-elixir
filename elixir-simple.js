fc8670c2270830b12bc956ad5af8a110e7aca661
Lorenzo Manacorda <lorenzo@mailbox.org>
2016-01-22 17:31:39 +0100

    handle _

--- elixir-simple.js
+++ elixir-simple.js
@@ -26,6 +26,8 @@ CodeMirror.defineSimpleMode("elixir", {
     {regex: /[\{\[\(]|%{|do|->/, indent: true},
     {regex: /[\}\]\)]|end/, dedent: true},
     {regex: /[a-z][\w]*/, token: "variable"},
+    // underscored variables
+    {regex: /_\w*/, token: "comment"},
     // You can embed other modes with the mode property. This rule
     // causes all code between << and >> to be highlighted with the XML
     // mode.
