359fd38a12a1b9b6b2b41a6d8ab217d5a3edac13
Lorenzo Manacorda <lorenzo@mailbox.org>
2016-01-24 00:37:25 +0100

    reoder operator
    i think the reason why -> wouldn't indent was that - was being matched
    first

--- elixir-simple.js
+++ elixir-simple.js
@@ -21,10 +21,10 @@ CodeMirror.defineSimpleMode("elixir", {
     {regex: /\/(?:[^\\]|\\.)*?\//, token: "variable-3"},
     // A next property will cause the mode to move to a different state
     // {regex: /\/\*/, token: "comment", next: "comment"},
-    {regex: /[-+\/*=<>!]+/, token: "operator"},
     // indent and dedent properties guide autoindentation
     {regex: /(?:{|\[|\(|%{|do|->)/, indent: true},
     {regex: /[\}\]\)]|end/, dedent: true},
+    {regex: /[-+\/*=<>!]+/, token: "operator"},
     {regex: /[a-z][\w]*/, token: "variable"},
     // underscored variables
     {regex: /_\w*/, token: "comment"},
